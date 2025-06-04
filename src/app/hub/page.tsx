432502d7f13ad051672ab0c86fdf6346fa5d468f
Jasdeep Singh <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2025-06-04 02:06:46 -0400

    Update hub layout

--- src/app/hub/page.tsx
+++ src/app/hub/page.tsx
@@ -1,21 +1,3 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-
-export default function Lobby() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-        </header>
-        <Separator/>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+export default function Hub() {
+  return ("1123")
 }
